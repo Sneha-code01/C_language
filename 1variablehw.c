31c6b1f2d5322c2241cd42f32a8634737dee0680
Sneha-code01 <shrutisneha31@gmail.com>
2025-07-11 00:40:35 +0530

    Add files via upload

---
+++ 1variablehw.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    printf("enter the length of RECTANGLE\n");
+    float length;
+    scanf("%f", &length);
+    printf("enter the breadth of RECTANGLE\n");
+    float breadth;
+    scanf("%f", &breadth);
+    printf("area of RECTANGLE:%f\n", length*breadth);//area of ractangle
+    printf("enter the side of CUBE\n");
+    float side;
+    scanf("%f", &side);
+    printf("volume of CUBE%f\n", side*side*side);/*volume of 
+    cube*/
+    return 0;
+}
