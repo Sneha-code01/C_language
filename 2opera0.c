7de2f3f76d20d41a577a1a89b7db3230bd7924a5
Sneha-code01 <shrutisneha31@gmail.com>
2025-07-13 20:36:25 +0530

    Add files via upload

---
+++ 2opera0.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    int a= 1.99999;// output 1 aaya .999 ko completly hata dia round off nhi karta hai
+    printf("%d\n", a);
+    int b = 3;
+    int c = 4;
+    b -=c;//b-c hoga
+    printf("%d\n", b);
+    printf("%d\n", 3>9 && 4<9);
+    printf("%d\n", pow(5,6));
+    return 0;
+}
